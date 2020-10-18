e34ed2dcfa6d2290d348db86eae812e84037492c
Moritz Klein <Moritz.Klein@stud.h-da.de>
2020-10-19 01:38:26 +0200

    True initial commit

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody body;
+    public float walkSpeed = 10;
+
+    PlayerControls controls;
+    Vector2 move;
+    Vector2 look;
+    bool fire = false;
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled  += ctx => move = Vector2.zero;
+
+        controls.Gameplay.Look.performed += ctx => look = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Look.canceled  += ctx => look = Vector2.zero;
+
+        controls.Gameplay.Fire.performed += ctx => fire = true;
+        controls.Gameplay.Fire.canceled  += ctx => fire = false;
+    }
+
+
+    void FixedUpdate()
+    {
+        body.AddForce(new Vector3(move.x, 0, move.y) * walkSpeed);
+    }
+
+    void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+}
